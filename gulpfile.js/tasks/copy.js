fe29914eacd56411ac7e9c9a0bc11678c1e0728f
Arden de Raaij <a.de.raaij@gmail.com>
2015-06-30 20:25:04 +0200

    use gulp changed plugin to only upload changed files after first build

--- gulpfile.js/tasks/copy.js
+++ gulpfile.js/tasks/copy.js
@@ -7,12 +7,15 @@ var
 gulp.task('copy', function() {
 
   gulp.src(config.layout_src)
+  .pipe(plugins.changed(config.layout_build, {extension: '.liquid'}))
   .pipe(gulp.dest(config.layout_build));
 
   gulp.src(config.snippets_src)
+  .pipe(plugins.changed(config.snippets_build, {extension: '.liquid'}))
   .pipe(gulp.dest(config.snippets_build));
 
   gulp.src(config.templates_src)
+  .pipe(plugins.changed(config.templates_build, {extension: '.liquid'}))
   .pipe(gulp.dest(config.templates_build));
 
   // fonts and font icons
@@ -22,6 +25,7 @@ gulp.task('copy', function() {
 
   // Shopify config files
   gulp.src(config.config_src)
+  .pipe(plugins.changed(config.config_build))
   .pipe(gulp.dest(config.config_build));
 
 });
